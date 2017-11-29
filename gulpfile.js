d5cfca408753903ac5ad9117d234ac4130f9e0ff
ranzhu123 <986364200@qq.com>
2017-11-29 23:41:21 +0800

    add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp = require('gulp');
-var path = require('path');
+const gulp = require('gulp');
+const path = require('path');
 const shell = require('gulp-shell');
 gulp.task('upload', () => {
   gulp.src('')
     .pipe(shell([
-        'scp /dist root @http://47.92.112.44:/opt/test'
+        `scp -r ${path.resolve()}/dist root@47.92.112.44:/opt/test`
     ]));
 })
